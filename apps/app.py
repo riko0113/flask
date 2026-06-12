09670a69ec02ff6bed717816b8148bd40762c4bc
riko0113 <hrs2521407@stu.o-hara.ac.jp>
2026-06-12 14:32:12 +0900

    add apps

--- apps/app.py
+++ apps/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from pathlib import Path
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from apps.config import config
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
