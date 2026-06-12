0cb8a8157e949ef403739ad46d711e8c50fb7b42
ishida972 <hrs2521472@stu.o-hara.ac.jp>
2026-06-12 12:05:34 +0900

    add apps

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from apps.crud.forms import UserForm
+from flask import Blueprint, render_template, redirect, url_for
 
 crud= Blueprint(
     "crud",
@@ -10,3 +11,21 @@ crud= Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/users/new", methods=["GET","POST"])
+def create_user():
+    form = UserForm()
+
+    if form.validate_on_submit():
+
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html, form=form")
