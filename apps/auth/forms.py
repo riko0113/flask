b93c354f1efab2adddb4825c1d46732f44befe22
hrs-2521412 <hrs2521412@stu.o-hara.ac.jp>
2026-06-12 14:52:36 +0900

    update

--- apps/auth/forms.py
+++ apps/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-class SginUpForm(FlaskForm):
+class SignUpForm(FlaskForm):
     username = StringField(
         "ユーザー名",
         validators=[
