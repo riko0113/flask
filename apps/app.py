2fa8b83ecc450f4f94ae164d1b1602ca9fd60b5f
ishida972 <hrs2521472@stu.o-hara.ac.jp>
2026-06-12 14:37:28 +0900

    update

--- apps/app.py
+++ apps/app.py
@@ -14,14 +14,14 @@ def create_app(config_key):
 
     app.config.from_object(config[config_key])
 
-    csef.init_app(app)
+    csrf.init_app(app)
     db.init_app(app)
     Migrate(app, db)    
 
     login_manager.init_app(app)
 
     from apps.crud import views as crud_views
-    form apps.auth import views as auth_views
+    from apps.auth import views as auth_views
 
     app.register_blueprint(auth_views.auth, url_prefix="/auth")
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
