079e52536c029895ddbab37b6e681811c792bcb6
riko0113 <hrs2521407@stu.o-hara.ac.jp>
2026-06-12 11:53:15 +0900

    add apps

--- apps/app.py
+++ apps/app.py
@@ -1,25 +1,20 @@
-from pathlib import Path
 from flask import Flask
 from apps.config import config
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlshemy
+from flask_wtf.csrf import CSRFProtect
 
-#SQLAlchemyをインスタンス化
-db = SQLAlshemy()
+csrf = CSRFProtect()
 
 def create_app(config_key):
     app = Flask(__name__)
 
-
-    db.init_app(app)
-    Migrate(app, db)
     app.config.from_object(config[config_key])
+
+    csef.init_app(app)
+    db.init_app(app)
+    Migrate(app, db)    
+
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
-
-    from apps.detector import views as dt_views
-
-    app.register_blueprint(dt_views.dt)
 
     return app
