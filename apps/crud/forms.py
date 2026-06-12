6f84c62e85d9bf331f1af2ee1dd85e441604db6c
riko0113 <hrs2521407@stu.o-hara.ac.jp>
2026-06-12 11:59:18 +0900

    add apps

--- apps/crud/forms.py
+++ apps/crud/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, DateField
 from wtforms.validators import DataRequired, Email, Length
 
 
@@ -10,6 +10,12 @@ class UserForm(FlaskForm):
             DataRequired(message="ユーザー名は必須です。"),
             Length(max=30, message="30文字以内で入力してください。"),
         ],
+    )
+
+    birthday = DateField(
+        "生年月日",
+        validators=[DataRequired(message="生年月日は必須です。")],
+        format='%Y-%m-%d'  # HTMLの <input type="date"> と合わせる設定
     )
 
     email = StringField(
