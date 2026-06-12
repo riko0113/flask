3dc0df1403e7671c6aa72127a5fcdc2452d874be
hrs-2521412 <hrs2521412@stu.o-hara.ac.jp>
2026-06-12 11:28:38 +0900

    add apps

--- apps/config.py
+++ apps/config.py
@@ -6,6 +6,7 @@ basedir = Path(__file__).parent.parent.parent
 class BaseConfig:
     SECRET_KEY = "ffa61fa8069020b2501550ebe83ff105"
     WTF_CSRF_SECRET_KEY = "5a5f0d6325d5e615a77ec3e875af9271"
+    UPLOAD_FOLDER = str(Path(basedir, "apps", "images"))
 
 class LocalConfig(BaseConfig):
     SQLALCHEMY_DARABASE_URI = f"sqlite:///{basedir/'local.sqlite'}"
