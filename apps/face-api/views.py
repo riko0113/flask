0a8475513cf464753f206ea83bf92f15b260e00d
hrs-2521412 <hrs2521412@stu.o-hara.ac.jp>
2026-06-19 10:13:24 +0900

    update

---
+++ apps/face-api/views.py
@@ -0,0 +1,19 @@
+from apps.app import db
+from apps.crud.models import User
+from apps.crud.forms import UserForm
+from apps.crud.models import User
+from flask import Blueprint, render_template, redirect, url_for
+from flask import abort
+from flask_login import current_user, login_required
+
+face= Blueprint(
+    "face",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+@crud.route("/")
+@login_required
+def index():
+    return render_template("face/snipet.html")
