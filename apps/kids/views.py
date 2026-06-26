d57d966a53533f56c3a599c1ff6b0d7131279ab1
ishida972 <hrs2521472@stu.o-hara.ac.jp>
2026-06-26 11:22:42 +0900

    add apps

--- apps/kids/views.py
+++ apps/kids/views.py
@@ -1,6 +1,6 @@
 from apps.app import db
 from apps.crud.models import User
-from apps.Kids.models import UserImage
+from apps.Kids.models import KidsImage
 from flask import Blueprint, render_template
 
 dt = Blueprint(
@@ -12,9 +12,9 @@ dt = Blueprint(
 @dt.route("/")
 def index():
     user_images= (
-        db.session.query(User, UserImage)
-        .join(UserImage)
-        .filter(User.id == UserImage.user_id)
+        db.session.query(User, KidsImage)
+        .join(KidsImage)
+        .filter(User.id == KidsImage.user_id)
         .all()
     )
     return render_template("Kids/index.html", user_images=user_images)
