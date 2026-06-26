ac83373ac44fe5378977aa58f16d5a88e33d3383
ishida972 <hrs2521472@stu.o-hara.ac.jp>
2026-06-26 11:04:03 +0900

    add apps

---
+++ apps/kids/views.py
@@ -0,0 +1,20 @@
+from apps.app import db
+from apps.crud.models import User
+from apps.Kids.models import UserImage
+from flask import Blueprint, render_template
+
+dt = Blueprint(
+    "Kids",
+    __name__,
+    template_folder="templates",
+)
+
+@dt.route("/")
+def index():
+    user_images= (
+        db.session.query(User, UserImage)
+        .join(UserImage)
+        .filter(User.id == UserImage.user_id)
+        .all()
+    )
+    return render_template("Kids/index.html", user_images=user_images)
