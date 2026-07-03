bc5134669495b5088916981113581843a0ae2cd4
hrs-2521412 <hrs2521412@stu.o-hara.ac.jp>
2026-07-03 10:18:14 +0900

    update

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from apps.app import db
 from apps.crud.models import User
 from apps.detector.models import UserImage
-from apps.detector.forms import UploadImageForm, DeleteForm
+from apps.detector.forms import UploadImageForm, DeleteForm, EditForm
 from flask import (
     Blueprint,
     current_app,
@@ -77,3 +77,34 @@ def delete_image(image_id):
     db.session.commit()
 
     return redirect(url_for("detector.index"))
+
+@detector.route("/edit/<int:image_id>", methods=["GET", "POST"])
+@login_required
+def edit_image(image_id):
+    image = UserImage.query.get_or_404(image_id)
+
+    # 自分の投稿だけ編集可能
+    if image.user_id != current_user.id:
+        return redirect(url_for("detector.index"))
+    form = EditImageForm(obj=image)
+
+    if form.validate_on_submit():
+        # ジャンル・コメントを更新
+        image.genre = form.genre.data
+        image.comment = form.comment.data
+        # 画像が選択された場合のみ更新
+        if form.image.data:
+            file = form.image.data
+            ext = Path(file.filename).suffix
+            filename = str(uuid.uuid4()) + ext
+            image_path = Path(
+                current_app.config["UPLOAD_FOLDER"],
+                filename
+            )
+            file.save(image_path)
+            image.image_path = filename
+
+        db.session.commit()
+        return redirect(url_for("detector.index"))
+
+    return render_template("detector/edit.html", form=form,  image=image)
